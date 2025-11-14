51f6540e5b552ef809dbaab362de97f1e9e29f28
Ania <anna111239@gmail.com>
2025-11-14 21:28:11 +0100

    update mdb

--- src/db/connectMongoDB.js
+++ src/db/connectMongoDB.js
@@ -1,12 +1,8 @@
-// src/db/connectMongoDB.js
 import mongoose from 'mongoose';
 
 export const connectMongoDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('✅ MongoDB connection established successfully');
   } catch (error) {
     console.error('❌ MongoDB connection failed:', error.message);
